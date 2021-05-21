74426c4638efdeca522ef759bcd9c27ac195b0f6
gunpreetsingh007 <alanwalker7172@gmail.com>
2021-05-21 19:09:16 +0530

    Slider working

--- js.js
+++ js.js
@@ -7,34 +7,36 @@ $(document).ready(function() {
     });
     $('.your-class').slick({
         dots: true,
-        infinite: true,
+        infinite: false,
         speed: 300,
         slidesToShow: 4,
-        arrows: true,
         slidesToScroll: 4,
-        responsive: [{
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 4,
-                    slidesToScroll: 4,
-                    infinite: true,
-                    dots: true
-                }
-            }, {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 4,
-                    slidesToScroll: 4
-                }
-            }, {
-                breakpoint: 369,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
             }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
         ]
-    });
+      });
 });
 // Slick
 
